829a298abde38010f86da9610c47b44932f55336
duuunjaaa <62660866+duuunjaaa@users.noreply.github.com>
2023-12-22 21:51:15 +0100

    bez iskomentarisanog

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ while not done:
     game.player = 1
 
     while(not game.done):
-        # turn = input('Enter X action: ')
         pos_i, pos_j = input("Enter two numbers here: ").split()
         pos_i, pos_j = [int(pos_i), int(pos_j)]
         player_action = TicTacToeAction(pos_i, pos_j)
@@ -19,26 +18,8 @@ while not done:
         game.render()
         if done:
             break
-        
+
         comp_action = minimax.minimax(game, player_action)[1]
         board, done, reward = game.step(comp_action)
         game.render()
-        # if(game.player == 1):
-        #     turn = input('Enter X action: ')
-        #     pos = tuple(int(x) for x in turn.split(","))
-        #     # pos_i =
-        #     # pos_j = 
-        #     player_action = TicTacToeAction(pos)
-        #     action = player_action
-        #     # board, done, reward = game.step(player_action)
-        # else:
-        #     comp_action = minimax.minimax(game, player_action)[1]
-        #     action = comp_action
-        #     # board, done, reward = game.step(comp_action)
-        # board, done, reward = game.step(action)
-        # game.render()
-    # action = choice(actions)
-    # board, done, reward = game.step(action)
-
-    # game.render()
     print("Reward:", reward)
--- solvers/Minimax.py
+++ solvers/Minimax.py
@@ -7,13 +7,10 @@ class Minimax:
     def minimax(self, game: Game, prev_action) -> tuple[float, any]:
         if game.done: 
             return game.get_state_value(), prev_action
-        # game2 = deepcopy(game)
         actions = game.get_valid_actions()
         best_action = None
         if game.player == 1:
             value = float("-inf")
-            # game2 = deepcopy(game)
-            # actions = game2.get_valid_actions()
             for action in actions:
                 game2 = deepcopy(game)
                 game2.step(action)
@@ -21,11 +18,8 @@ class Minimax:
                 if value < curr_value:
                     best_action = action
                 value = max(value, curr_value)
-            # return value
         else:
             value = float("inf")
-            # game2 = deepcopy(game)
-            # actions = game2.get_valid_actions()
             for action in actions:
                 game2 = deepcopy(game)
                 game2.step(action)
@@ -33,6 +27,5 @@ class Minimax:
                 if value > curr_value:
                     best_action = action
                 value = min(value, curr_value)
-            # return value
         return value, best_action
         
